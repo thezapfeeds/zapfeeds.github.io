16d5f09d87c2e8bb18160d70278a08bebad11493
Ishan Saini <isaini331@gmail.com>
2024-10-28 22:32:06 +0530

    sample_app

---
+++ index.js
@@ -0,0 +1,64 @@
+ringSounds();
+keySounds();
+
+function ringSounds() {
+    let keys = document.querySelectorAll(".drum");
+    for(let i = 0; i < keys.length; i++) {
+        keys[i].addEventListener("click", function() {
+            var key = keys[i].innerHTML;
+            console.log("The key is "+key);
+            drumButtonCases(key);
+            buttonAnimation(key);
+        });
+    }
+}
+
+function keySounds(){
+    document.addEventListener('keydown',function(event){
+        var key = event.key;
+        drumButtonCases(key);
+        buttonAnimation(key);
+    });
+}
+
+function drumButtonCases(key){
+    switch(key){
+        case "w":
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        default : 
+            console.log("Not a valid option for a drum key !");    
+    }
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add('pressed');
+    setTimeout(function(){ activeButton.classList.remove('pressed') }, 100);
+}
+
